2c1e34ffc015da72f666b4963546875f6c3194f9
Nikhil-evol <nikhil95124@gmail.com>
2026-08-17 18:23:56 +0530

    utilities updated

---
+++ src/utils/utilities.py
@@ -0,0 +1,18 @@
+import tiktoken
+
+
+def count_num_tokens(text: str, model: str) -> int:
+    """
+    Returns the number of tokens in the given text.
+    Args:
+        text (str): The text to count tokens in.
+        model (str, optional): The name of the GPT model to use. Defaults to the model specified in the app config.
+
+    Returns:
+        int: The number of tokens in the text.
+    """
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    return len(encoding.encode(text))
